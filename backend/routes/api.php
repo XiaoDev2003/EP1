59ced790497e3b3de3c886e3ae804906f23fc573
vinh267 <vinhnguyenthanh267@gmail.com>
2025-04-26 22:13:54 +0700

    reup

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -32,10 +32,6 @@ switch (true) {
         require_once __DIR__ . '/../controllers/QueryController.php';
         break;
 
-    case preg_match('/\/api\/visitor/', $request):
-        require_once __DIR__ . '/../controllers/VisitorController.php';
-        break;
-
     default:
         http_response_code(404);
         echo json_encode(['error' => 'API route not found']);
